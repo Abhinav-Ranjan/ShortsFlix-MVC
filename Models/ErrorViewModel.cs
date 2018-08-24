148340efec7f042825f3a1510148430f8896aa84
Abhinav Ranjan <abhinav.ranjan@gmail.com>
2018-08-23 19:58:58 -0700

    New Project 'ShortsFlix-MVC' is being added

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ShortsFlix_MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
